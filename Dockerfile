424a0b306922ed0702fde2cdea363370afc6cdf7
Ryan Schlesinger <ryan@ryanschlesinger.com>
2016-05-20 10:18:32 -0700

    Add instance id and type to tags

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM gliderlabs/registrator:v7
 MAINTAINER Ryan Schlesinger <ryan@outstand.com>
 
+RUN apk update && \
+      apk add bash && \
+      rm -rf /var/cache/apk/*
+
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 ENTRYPOINT ["/docker-entrypoint.sh"]
 CMD ["registrator"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 if [ "${1:0:1}" = '-' ]; then
@@ -9,11 +9,42 @@ if expr "$1" : '[A-Za-z0-9_-]*://' >/dev/null; then
     set -- registrator "$@"
 fi
 
+function join { local IFS="$1"; shift; echo "$*"; }
+
 if [ "$1" = 'registrator' ]; then
-  if [ -n "$TAG_AWS_AZ" ]; then
+  if [ -n "$TAG_AWS" ]; then
+    echo "==> Looking for AWS metadata"
+    declare -a TAGS
+
+    set +e
+
     AZ=$(wget -O - -q http://169.254.169.254/latest/meta-data/placement/availability-zone)
+    if [ $? -eq 0 ]; then
+      TAGS+=(${AZ})
+    fi
+
+    INSTANCE_ID=$(wget -O - -q http://169.254.169.254/latest/meta-data/instance-id)
+    if [ $? -eq 0 ]; then
+      TAGS+=(${INSTANCE_ID})
+    fi
+
+    INSTANCE_TYPE=$(wget -O - -q http://169.254.169.254/latest/meta-data/instance-type)
+    if [ $? -eq 0 ]; then
+      TAGS+=(${INSTANCE_TYPE})
+    fi
+
+    set -e
+
+    TAGS_STR=""
+    if [ ${#TAGS[@]} -gt 0 ]; then
+      TAGS_STR="-tags $(join , "${TAGS[@]}")"
+      echo "==> Running registrator with ${TAGS_STR}"
+    else
+      echo "==> No AWS metadata found"
+    fi
+
     shift
-    set -- registrator -tags "$AZ" "$@"
+    set -- registrator ${TAGS_STR} "$@"
   fi
 fi
 
